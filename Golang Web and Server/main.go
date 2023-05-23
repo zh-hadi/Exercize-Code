0d3528908f748d353d17a627d012898ac1efae83
Hadiuzzaman Hadi <zhhadi50@gmail.com>
2023-05-23 13:06:19 +0600

    Connect MySql server and Read Data in Data Base

--- Golang Web and Server/main.go
+++ Golang Web and Server/main.go
@@ -1,18 +1,44 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
 
 func main() {
+	// server connection
+	db, err := sql.Open("mysql", "root:2023@(127.0.0.1:3306)/rony?parseTime=true")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	// read data form server
+	var (
+		id        int
+		postTitle string
+		content   string
+	)
+
+	query := "SELECT id, title, content FROM post WHERE id = 1"
+	if err := db.QueryRow(query).Scan(&id, &postTitle, &content); err != nil {
+		log.Fatal(err)
+	}
 
 	r := mux.NewRouter() //
 
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello and welcome to golang server\n use gorilla mux router services")
+	})
+	r.HandleFunc("/post", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, postTitle+"\n"+content)
 	})
 
 	http.ListenAndServe(":4001", r) // fist parameter port number
