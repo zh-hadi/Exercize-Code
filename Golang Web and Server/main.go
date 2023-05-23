28b5a841f381efd18e6e7329df1a0109ed363b49
Hadiuzzaman Hadi <zhhadi50@gmail.com>
2023-05-23 12:38:04 +0600

    create go server with gorilla mux router

--- Golang Web and Server/main.go
+++ Golang Web and Server/main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello and welcome to golang server")
+
+	r := mux.NewRouter() //
+
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello and welcome to golang server\n use gorilla mux router services")
 	})
 
-	http.ListenAndServe(":4001", nil) // fist parameter port number
+	http.ListenAndServe(":4001", r) // fist parameter port number
 }
